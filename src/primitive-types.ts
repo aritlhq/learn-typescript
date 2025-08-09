f9cd90760e0057543ed7081bfd18b2e2f6b5cf5e
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-08-10 05:05:03 +0700

    feat: Add basic primitive types example

---
+++ src/primitive-types.ts
@@ -0,0 +1,13 @@
+/*
+String
+Number
+Boolean
+ */
+
+const name:string = "John Doe";
+const age:number = 30;
+const isMarried: boolean = true;
+
+console.log(name);
+console.log(age);
+console.log(isMarried);
