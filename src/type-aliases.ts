81d4ecee752f8b80f25b9aba27385f924dc07b13
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-08-10 05:05:28 +0700

    feat: Add tuple-type and type-aliases examples

---
+++ src/type-aliases.ts
@@ -0,0 +1,16 @@
+/*
+Alias Data Type
+ */
+
+export type Category = {
+    id: number,
+    name: string
+    description: string
+}
+
+export type Product = {
+    id: number,
+    name: string,
+    price: number,
+    category: Category
+}
