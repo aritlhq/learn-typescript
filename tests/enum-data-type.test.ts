40d4e5f19824825346060610bb2ff2e08a10fc77
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-08-10 05:03:59 +0700

    feat: Add babel config and enum data type test

---
+++ tests/enum-data-type.test.ts
@@ -0,0 +1,19 @@
+import {type Customer, CustomerStatus, CustomerType} from "../src/enum-type";
+
+describe("Enum Data Type", () => {
+    it("Should check enum", () => {
+        const customer: Customer = {
+            name: "John Doe",
+            age: 30,
+            type: CustomerType.GOLD,
+            status: CustomerStatus.ACTIVE
+        }
+
+        console.log(customer);
+
+        expect(customer.name).toBe("John Doe");
+        expect(customer.age).toBe(30);
+        expect(customer.type).toBe(CustomerType.GOLD);
+        expect(customer.status).toBe("ACTIVE");
+    })
+})
