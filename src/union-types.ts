f0a3186f4ceef9c919ba700372a80703dc67886e
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-08-10 05:05:47 +0700

    feat: Add union type examples
    This commit introduces examples for union types in TypeScript.

---
+++ src/union-types.ts
@@ -0,0 +1,14 @@
+export type ID = string | number;
+
+export type Category = {
+    id: ID,
+    name: string
+    description: string
+}
+
+export type Product = {
+    id: number,
+    name: string,
+    price: number,
+    category: Category
+}
