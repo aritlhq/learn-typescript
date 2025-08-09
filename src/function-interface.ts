411ab3bea3bfb72823cae5be4299241cd8ce85c9
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-08-10 05:04:13 +0700

    feat: Add enum-type and function-interface files

---
+++ src/function-interface.ts
@@ -0,0 +1,13 @@
+/*
+Function Interface
+ */
+
+interface AddFunction {
+    (value1: number, value2: number): number
+}
+
+const add: AddFunction = function (value1: number, value2: number) {
+    return value1 + value2
+}
+
+console.log(add(10, 20));
