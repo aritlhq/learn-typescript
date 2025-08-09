00464cb5a2f95256f09eaec74e8eeadf7341997d
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-08-10 05:03:38 +0700

    feat: Add alias data type tests

---
+++ tests/alias-data-type.test.ts
@@ -0,0 +1,26 @@
+import type {Category, Product} from "../src/type-aliases";
+
+describe('Alias Data Type', () => {
+    it('Should check alias', () => {
+        const category: Category = {
+            id: 1,
+            name: "Category 1",
+            description: "Description 1"
+        }
+
+        const product: Product = {
+            id: 1,
+            name: "Product 1",
+            price: 100,
+            category
+        }
+
+        console.log(product);
+
+        expect(category.id).toBe(1);
+        expect(product.name).toBe("Product 1");
+        expect(product.category.id).toBe(1);
+        expect(product.name).toBe("Product 1");
+        expect(product.price).toBe(100);
+    })
+});
