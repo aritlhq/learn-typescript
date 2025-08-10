3fae12a903e12f875ce60407ab47fc2700d7fa10
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-08-10 10:46:12 +0700

    feat: Add indexable interface tests

---
+++ tests/indexable-interface.test.ts
@@ -0,0 +1,19 @@
+import type {Product, Category} from "../src/indexable-interface";
+
+describe('Indexable Interface', () => {
+    it("Should check indexable interface", () => {
+        const product: Product = ["Product 1", "Product 2", "Product 3"];
+        console.log(product[2]);
+
+        const category: Category = {
+            "1": "Category 1",
+            "2": "Category 2",
+            "3": "Category 3",
+        }
+        console.log(category["2"]);
+
+        expect(product[2]).toBe("Product 3");
+        expect(category["2"]).toBe("Category 2");
+    })
+
+});
