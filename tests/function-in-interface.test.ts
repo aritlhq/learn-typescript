3ac022d03c84406e528c54bb355ee1237d477b08
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-08-10 10:45:51 +0700

    feat: Add test case for function in interface

---
+++ tests/function-in-interface.test.ts
@@ -0,0 +1,14 @@
+import type {Person} from "../src/function-in-interface";
+
+describe('Function in Interface', () => {
+    it("Should check function in interface", () => {
+        const person: Person = {
+            name: "John Doe",
+            sayHello: (name: string) => `Hello ${name}`
+        }
+
+        console.log(person.sayHello("Jane Doe"));
+
+        expect(person.sayHello("Jane Doe")).toBe("Hello Jane Doe");
+    })
+});
