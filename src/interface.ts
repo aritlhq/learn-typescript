5f88a8d5402a8792e05aa5aa3230ff9825400dc9
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-08-10 10:46:29 +0700

    refactor(interface): Define Category interface with export

--- src/interface.ts
+++ src/interface.ts
@@ -1,13 +1,9 @@
-interface Category {
+/*
+Interface
+ */
+
+export interface Category {
     id: number
     name?: string
     readonly description: string
 }
-
-
-const category: Category = {
-    id: 1,
-    description: "Description 1"
-}
-
-console.log(category)
