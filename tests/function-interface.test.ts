c62a51a26ce7a3e181ee1a897fc08bfdb4dbb97d
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-08-10 10:46:02 +0700

    feat: Add function interface test

---
+++ tests/function-interface.test.ts
@@ -0,0 +1,13 @@
+import type {AddFunction} from "../src/function-interface";
+
+describe("Function Interface", () => {
+    it("Should check function", () => {
+        const add: AddFunction = function (value1: number, value2: number) {
+            return value1 + value2
+        }
+
+        console.log(add(1, 2));
+
+        expect(add(1, 2)).toBe(3);
+    })
+})
