3e024248d5664df9e9efe49ca7f6ac86110f0c70
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-08-10 10:47:10 +0700

    feat: Add type assertion tests
    tests: Add type assertion test file

---
+++ tests/type-assertion.test.ts
@@ -0,0 +1,24 @@
+describe('Type Assertion', () => {
+    it("Should check type assertion", () => {
+        const person: any = {
+            name: "John Doe",
+            age: 30
+        }
+
+        const personName = person.name as string;
+        const personAge = person.age as number;
+
+        console.log(personName);
+        console.log(personAge);
+    })
+
+    it("Should check type assertion", () => {
+        const person: any = {
+            name: "John Doe",
+            age: 30
+        }
+
+        const personIdentity: Person = person as Person;
+        console.log(personIdentity);
+    })
+});
