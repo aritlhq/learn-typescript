eb79cec931c0f63552e57200eafb853793ae16d0
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-08-11 11:07:16 +0700

    feat: Add class.test.ts for class testing

---
+++ tests/oop/class.test.ts
@@ -0,0 +1,12 @@
+describe('Class', () => {
+    it("Should check class", () => {
+        class Customer {
+        }
+
+        class Order {
+        }
+
+        const customer: Customer = new Customer();
+        const order: Order = new Order();
+    })
+});
