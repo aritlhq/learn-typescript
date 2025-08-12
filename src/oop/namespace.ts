38e1b696b7fae5ce2bdff43bdf20abe6ea66300c
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-08-12 23:55:32 +0700

    feat(namespace): Add MathUtil namespace with PI and sum function

---
+++ src/oop/namespace.ts
@@ -0,0 +1,13 @@
+export namespace MathUtil {
+    export const PI: number = 3.14;
+
+    export function sum(...values: number[]): number {
+        let total = 0;
+
+        for (let value of values) {
+            total = total + value;
+        }
+
+        return total;
+    }
+}
