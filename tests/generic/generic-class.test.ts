3150077b638fd6ccbae968caafd40f7409dfe73c
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-08-13 09:31:40 +0700

    feat: Add generic class test

---
+++ tests/generic/generic-class.test.ts
@@ -0,0 +1,19 @@
+import {Data} from "../../src/generic/generic-class";
+
+describe("Generic Class", () => {
+    it('Should check generic', () => {
+        // Create object
+        const data: Data<any> = new Data<number>(1234);
+
+        data.value = "Jane Doe";
+        data.value = true;
+
+        console.log(data);
+
+        // Creating data in generi
+        const dataString: Data<any> = new Data<string>("Jane Doe 2");
+        const firstName = dataString.value.substring(0, 3);
+
+        console.log(firstName);
+    });
+})
